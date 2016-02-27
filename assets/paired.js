4063c674cbeefbede49e920d82fbe9de98134837
Sho Iizuka <sho.i518@gmail.com>
2016-02-27 16:51:53 +0900

    Implemented paired t-test

--- assets/paired.js
+++ assets/paired.js
@@ -1,4 +1,17 @@
 $(function () {
+    var pairedttest = function (d) {
+        var distributions = require("distributions");
+        var t = d.mean() * Math.sqrt(d.size()) / d.sd();
+        var nu = d.size() - 1;
+        var studentt = new distributions.Studentt(nu);
+        var pvalue = 2 * (1 - studentt.cdf(Math.abs(t)));
+        return { t: t, nu: nu, pvalue: pvalue };
+    };
+
+    var effectsizePaired = function (d) {
+        return d.mean() / d.sd();
+    };
+
     Number.isFinite = Number.isFinite || function (any) {
         return typeof any === 'number' && isFinite(any);
     };
@@ -16,39 +29,54 @@ $(function () {
         return summary(numbers);
     };
 
+    var difference = function (xs, ys) {
+        var summary = require("summary");
+        var d = [];
+        for (var i = 0; i < xs.length; i++) {
+            d.push(xs[i] - ys[i]);
+        }
+        return summary(d);
+    };
+
     $('#run-test').on('click', function () {
         var xs = parseTextArea("#group-x");
         var ys = parseTextArea("#group-y");
 
+        $("#size-x").text(xs.size());
+        $("#size-y").text(ys.size());
+
+        $("#mean-x").text(xs.mean().toPrecision(5));
+        $("#mean-y").text(ys.mean().toPrecision(5));
+
+        $("#variance-x").text(xs.variance().toPrecision(5));
+        $("#variance-y").text(ys.variance().toPrecision(5));
+
+        $("#sd-x").text(xs.sd().toPrecision(5));
+        $("#sd-y").text(ys.sd().toPrecision(5));
+
         if (xs.size() != ys.size()) {
             $("#alert-size").css("display", "block");
 
-            $("#size-x").text(xs.size());
-            $("#size-y").text(ys.size());
-
-            $("#mean-x").text(xs.mean().toPrecision(5));
-            $("#mean-y").text(ys.mean().toPrecision(5));
-
-            $("#variance-x").text(xs.variance().toPrecision(5));
-            $("#variance-y").text(ys.variance().toPrecision(5));
-
-            $("#sd-x").text(xs.sd().toPrecision(5));
-            $("#sd-y").text(ys.sd().toPrecision(5));
+            $("#mean-d").text("NA");
+            $("#variance-d").text("NA");
+            $("#paired-test-t").text("NA");
+            $("#paired-test-nu").text("NA");
+            $("#paired-test-p").text("NA");
+            $("#effect-size-paired").text("NA");
         }
         else {
             $("#alert-size").css("display", "none");
 
-            $("#size-x").text(xs.size());
-            $("#size-y").text(ys.size());
+            var d = difference(xs._data, ys._data);
 
-            $("#mean-x").text(xs.mean().toPrecision(5));
-            $("#mean-y").text(ys.mean().toPrecision(5));
-
-            $("#variance-x").text(xs.variance().toPrecision(5));
-            $("#variance-y").text(ys.variance().toPrecision(5));
-
-            $("#sd-x").text(xs.sd().toPrecision(5));
-            $("#sd-y").text(ys.sd().toPrecision(5));
+            $("#mean-d").text(d.mean().toPrecision(5));
+            $("#variance-d").text(d.variance().toPrecision(5));
+            var paired = pairedttest(d);
+            $("#paired-test-t").text(paired.t.toPrecision(5));
+            $("#paired-test-nu").text(paired.nu.toPrecision(5));
+            $("#paired-test-p").text(paired.pvalue.toPrecision(5));
+            var es = effectsizePaired(d);
+            $("#effect-size-paired").text(es.toPrecision(5));
         }
     });
 
--- assets/unpaired.js
+++ assets/unpaired.js
@@ -1,14 +1,11 @@
 $(function () {
     var welchttest = function (xs, ys) {
         var distributions = require("distributions");
-        var summary = require("summary");
-        var x = summary(xs);
-        var y = summary(ys);
-        var sxn = x.variance() / x.size();
-        var syn = y.variance() / y.size();
-        var t = (x.mean() - y.mean()) / Math.sqrt(sxn + syn);
+        var sxn = xs.variance() / xs.size();
+        var syn = ys.variance() / ys.size();
+        var t = (xs.mean() - ys.mean()) / Math.sqrt(sxn + syn);
         var numer = Math.pow(sxn + syn, 2);
-        var denom = Math.pow(sxn, 2) / (x.size() - 1) + Math.pow(syn, 2) / (y.size() - 1);
+        var denom = Math.pow(sxn, 2) / (xs.size() - 1) + Math.pow(syn, 2) / (ys.size() - 1);
         var nu = numer / denom;
         var studentt = new distributions.Studentt(nu);
         var pvalue = 2 * (1 - studentt.cdf(Math.abs(t)));
@@ -16,11 +13,8 @@ $(function () {
     };
 
     var effectsizeUnpaired = function (xs, ys) {
-        var summary = require("summary");
-        var x = summary(xs);
-        var y = summary(ys);
-        var numer = x.mean() - y.mean();
-        var denom = Math.sqrt(((x.size() - 1) * x.variance() + (y.size() - 1) * y.variance()) / (x.size() + y.size() - 2));
+        var numer = xs.mean() - xs.mean();
+        var denom = Math.sqrt(((xs.size() - 1) * xs.variance() + (xs.size() - 1) * xs.variance()) / (xs.size() + xs.size() - 2));
         return numer / denom;
     };
 
@@ -56,11 +50,12 @@ $(function () {
         $("#sd-x").text(xs.sd().toPrecision(5));
         $("#sd-y").text(ys.sd().toPrecision(5));
 
-        var welch = welchttest(xs._data, ys._data);
+        var welch = welchttest(xs, ys);
         $("#welch-ttest-t").text(welch.t.toPrecision(5));
         $("#welch-ttest-nu").text(welch.nu.toPrecision(5));
         $("#welch-ttest-p").text(welch.pvalue.toPrecision(5));
-        $("#effect-size-cohen").text(effectsizeUnpaired(xs._data, ys._data).toPrecision(5));
+        var es = effectsizeUnpaired(xs, ys);
+        $("#effect-size-unpaired").text(es.toPrecision(5));
     });
 
     $("#group-x").val([1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 4, 1, 1].join("\n"));
